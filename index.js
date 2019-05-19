2b4d8585a8a106e43a84c218147f864673e0fd07
Samantha brake <sammy.brake@gmail.com>
2019-05-19 15:40:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ class BoardMember {
   }
 
   approve() {
-    returns "You can do that!";
+    return "You can do that!";
   }
 
   doCharity() {
-    returns "I like to help people";
+    return "I like to help people";
   }
 
   releasePressStatement() {
-    returns "You will see great things from Scuber.";
+    return "You will see great things from Scuber.";
   }
 
   sayHi() {
-    returns `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`
+    return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`
   }
 };
